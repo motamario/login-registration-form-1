50821104381fa454d0673f897fcb09f4dc147e85
Mario Mota <rosasmario24@gmail.com>
2023-08-16 22:16:18 -0600

    Create Registration and Login component

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+import Register from './Register'
+import Login from './Login'
+
+function App() {
+  return (
+  <div className='app'>
+    <Register />
+    <Login />
+
+  </div>
+  )
+}
+
+export default App
---
+++ src/Login.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Login() {
+  return (
+    <div className='login'>
+        <form action="">
+            <input type="email" placeholder='email@address.com' />
+            
+
+        </form>
+
+
+    </div>
+  )
+}
+
+export default Login
